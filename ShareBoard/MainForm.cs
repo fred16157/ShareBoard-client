a570e2e7df4be3ea52c0838fc27dc788e5b0e361
강현구 <fred16157@gmail.com>
2021-05-19 23:38:20 +0900

    이미지 복사

--- ShareBoard/MainForm.cs
+++ ShareBoard/MainForm.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -25,18 +28,53 @@ namespace ShareBoard
             };
 
             Hook.GlobalEvents().OnCombination(assignment);
-            
         }
 
         void OnCopy()
         {
-            if (!Clipboard.ContainsText()) return;
-            string clipboardText = Clipboard.GetText();
-
+            ToastContentBuilder builder = new ToastContentBuilder().AddText("클립보드에 복사됨");
+            
+            //if(Clipboard.ContainsImage())
+            //{
+            //    Guid guid = Guid.NewGuid();
+            //    ResizeImage(Clipboard.GetImage(), 200, 200).Save(guid.ToString() + ".bmp");
+            //    builder.AddInlineImage(new Uri("file:///" + Environment.CurrentDirectory + guid.ToString() + ".bmp"));
+            //    File.Delete(guid.ToString() + ".bmp");
+            //}
+            /*else */
+            if (Clipboard.ContainsText())
+            {
+                builder.AddText(Clipboard.GetText());
+            }
             //딜레이가 없으면 E_UNEXPECTED 에러를 뱉는다
             //왜 딜레이가 있어야 실행되는지는 모르겠음
             Thread.Sleep(500);
-            new ToastContentBuilder().AddText("클립보드에 복사됨").AddText(clipboardText).Show();
+            builder.Show();
+        }
+
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
         }
     }
 }
